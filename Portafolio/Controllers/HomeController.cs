00df116cc4f9c1458a293c263688400412f5c036
Nabuconodossor <gaferoco@gmail.com>
2023-12-24 04:18:36 -0300

    Listado de proyectos

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,13 +15,46 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            var persona = new Persona()
-            {
-                Nombre = "Gabriel Rodriguez",
-                Edad = 32
-            };
-       
-            return View(persona);
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
+        }
+
+        private List<Proyecto> ObtenerProyectos()
+        {
+
+            return new List<Proyecto> {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Link= "https://amazon.com",
+                    ImagenURL = "/imagenes/amazon.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Paginas de noticias en React",
+                    Link= "https://nytimes.com",
+                    ImagenURL = "/imagenes/nyt.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link= "https://amazon.com",
+                    ImagenURL = "/imagenes/reddit.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en linea para comprar video juegos",
+                    Link= "https://store.steampowered.com",
+                    ImagenURL = "/imagenes/steam.png"
+                },
+
+
+                };
         }
 
         public IActionResult Privacy()
